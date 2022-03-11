1679c167c90e1a02a6031d1355e49ac56356c9dd
darlingsandra <darlingsandra@yandex.ru>
2022-03-11 19:09:36 +0700

    add start screen

---
+++ AwesomeGame/App/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  AwesomeGame
+//
+//  Created by Александра Широкова on 11.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
---
+++ AwesomeGame/Views/MainButton.swift
@@ -0,0 +1,33 @@
+//
+//  MainButton.swift
+//  AwesomeGame
+//
+//  Created by Александра Широкова on 11.03.2022.
+//
+
+import UIKit
+
+class MainButton: UIButton {
+    override var isEnabled: Bool {
+        didSet {
+            backgroundColor = isEnabled ? backgroundColor?.withAlphaComponent(1) :  backgroundColor?.withAlphaComponent(0.3)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setStyleCustom()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyleCustom()
+    }
+    
+    func setStyleCustom() {
+        self.layer.cornerRadius = self.frame.size.height / 3
+        self.backgroundColor = .blue
+        self.setTitleColor(.white, for: .normal)
+        self.setTitleColor(.white, for: .disabled)
+    }
+}
